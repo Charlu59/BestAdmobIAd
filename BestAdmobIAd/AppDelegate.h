5465dd5652a1e01b633def93b2e67e61533f7e5b
Charlu59 <charles-hubertbasuiau@MacBook-Pro-de-Charles-Hubert.local>
2013-11-18 09:06:46 +0100

    Initial Commit

---
+++ BestAdmobIAd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BestAdmobIAd
+//
+//  Created by Charles-Hubert Basuiau on 18/11/2013.
+//  Copyright (c) 2013 Charles-Hubert Basuiau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BestAdmobIAdTests/BestAdmobIAdTests.m
@@ -0,0 +1,34 @@
+//
+//  BestAdmobIAdTests.m
+//  BestAdmobIAdTests
+//
+//  Created by Charles-Hubert Basuiau on 18/11/2013.
+//  Copyright (c) 2013 Charles-Hubert Basuiau. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BestAdmobIAdTests : XCTestCase
+
+@end
+
+@implementation BestAdmobIAdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
